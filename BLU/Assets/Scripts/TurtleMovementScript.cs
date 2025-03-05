5716ff2af98a33254510d8044f14eb44a56feb8d
00Ari00 <arissahe3485@gmail.com>
2025-03-05 11:01:01 -0500

    fixed movement code

--- BLU/Assets/Scripts/TurtleMovementScript.cs
+++ BLU/Assets/Scripts/TurtleMovementScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 
 public class TurtleMovementScript : MonoBehaviour
 {
-    public Rigidbody2D turtleRigidBody;
+    public Rigidbody2D turtleRB;
     public float moveSpeed = 3;
     private Vector2 moveDirection;
 
@@ -32,11 +32,12 @@ public class TurtleMovementScript : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        // using .normalized to ensure diagonal movement is not faster than vertical and horizontal movement
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
 
     void Move()
     {
-        turtleRigidBody.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        turtleRB.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
