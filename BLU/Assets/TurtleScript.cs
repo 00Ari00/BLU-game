2028de4e5fb2dc4f00d694477fcb70d6212f15b3
filza07 <filza.hashim@gmail.com>
2025-06-09 11:53:43 -0400

    basic net game done
    most of the net game works fine now, but im adding touch ups

--- BLU/Assets/TurtleScript.cs
+++ BLU/Assets/TurtleScript.cs
@@ -5,19 +5,30 @@ public class TurtleScript : MonoBehaviour
 
     public Rigidbody2D myRigidBody;
     public float swimStrength;
+    public LogicScript logic;
+    public bool turtleIsAlive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKey(KeyCode.Space) == true && turtleIsAlive == true)
         {
-            myRigidBody.linearVelocity = Vector2.up * swimStrength;
+            myRigidBody.linearVelocity = new Vector2(myRigidBody.linearVelocity.x, swimStrength);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        turtleIsAlive = false;
+
+    }
+
 }
