5823f5ee4d6af455131fdc0081eaa71d38f6e669
Selinawf <selinawangf@gmail.com>
2025-06-12 12:58:52 -0400

    yes
    0

--- BLU/Assets/MouseInput.cs
+++ BLU/Assets/MouseInput.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
 
-public class MouseInput : MonoBehaviour
+public class MouseInput : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private MaterialApplier materialApplier;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log (Mouse 0 - Left Click);
+        }
+
+//comment
+    }
+
+    public void Awake() {
+        materialApplier = GetComponent<MaterialApplier>();
+    }
+
+    public void OnPointerDown (OnPointerEventData eventData){
+        materialApplier.ApplyOther();
+    }
+
+    public void OnPointerUp (OnPointerEventData eventData){
+        materialApplier.ApplyOriginal();
+    }
+
+    public void OnPointerClick (OnPointerEventData eventData){
+        //
+    }
+
+    public void OnPointerEnter (OnPointerEventData eventData){
+        //
+    }
+
+    public void OnPointerClick (OnPointerEventData eventData){
+        //
+    }
+
+    public void OnPointerExit (OnPointerEventData eventData){
+        //
     }
 }
