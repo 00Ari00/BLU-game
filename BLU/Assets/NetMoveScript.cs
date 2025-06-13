6df615b495e40ba0314ca4d3d48b36321443e5c9
filza07 <filza.hashim@gmail.com>
2025-06-13 07:28:54 -0400

    escape it basically done
    escape it is a running game now! I might add final touches but overall it's done!!!!! :D

--- BLU/Assets/NetMoveScript.cs
+++ BLU/Assets/NetMoveScript.cs
@@ -2,26 +2,38 @@ using UnityEngine;
 
 public class NetMoveScript : MonoBehaviour
 {
-
     public float moveSpeed = 5;
     public float deadZone = -20;
+    public float floatStrength;
+    public float floatSpeed;
+    public float originalY;
+    private float floatOffset;
+    private LogicScript logic;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        originalY = transform.position.y;
+        floatOffset = Random.Range(0f, 2f * Mathf.PI);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (!logic.isGameOver)
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
 
-        if(transform.position.x < deadZone)
+            float newY = originalY + Mathf.Sin(Time.time * floatSpeed + floatOffset) * floatStrength;
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        }
+
+        if (transform.position.x < deadZone)
         {
             Destroy(gameObject);
         }
-
     }
 }
