efcb3c9a742e42933a520cc2af86d755b0cdb5bf
Selinawf <selinawangf@gmail.com>
2025-06-16 21:49:50 -0400

    Textbox and turtle moving

--- BLU/Assets/Scripts/SelinaTurtleMovement.cs
+++ BLU/Assets/Scripts/SelinaTurtleMovement.cs
@@ -8,29 +8,31 @@ public class SelinaTurtleMovement : MonoBehaviour
     public float moveSpeed = 3;
     public float stopXPosition = 487;
     private bool isMoving = true;
-    
+    public TextBoxController textBoxController;
+    private bool messageShown = false;
+
     void Start()
     {
-     this.transform.position = new Vector3 (35.4f, -19.6f, -4);
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
+        this.transform.position = new Vector3(35.4f, -19.6f, -4);
+        turtleRB.linearVelocity = new Vector2(moveSpeed, 0);
     }
 
     // This function is called every fixed framerate frane, if MonoBehaviour is enabled
     void FixedUpdate()
     {
-        if (isMoving) {
-
+        if (isMoving && textBoxController != null && !textBoxController.TextBoxPanel.activeSelf)
+        {
             turtleRB.linearVelocity = new Vector2(moveSpeed, turtleRB.linearVelocity.y);
-        } 
-
-        if (transform.position.x >= stopXPosition) {
+        }
+        
+        if (transform.position.x >= stopXPosition && !messageShown)
+        {
             turtleRB.linearVelocity = Vector2.zero;
             isMoving = false;
-        }
-    }    
+            messageShown = true;
 
+            if (textBoxController != null)
+                textBoxController.ShowMessage("We've arrived at the reef!");
+        }
+    }
 }
--- BLU/Assets/Scripts/TextBoxController.cs
+++ BLU/Assets/Scripts/TextBoxController.cs
@@ -1,26 +1,56 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TextBoxController : MonoBehaviour
 {
-    public GameObject textboxPanel;
-    public Text messageText; // Or TMP_Text if using TextMeshPro
+    public GameObject TextBoxPanel; //Panel with background
+    public TMP_Text messageText; //Text
+    public float typingSpeed = 0.05f; //Time between letters
+    public float displayTime = 2f; //Time to display full message
+    private Coroutine typingCoroutine;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /*void Start()
     {
         textboxPanel.SetActive(false);
    
-    }
+    }*/
+
+
+
 
     public void ShowMessage(string message)
     {
-        messageText.text = message;
-        textboxPanel.SetActive(true);
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+           // messageText.text = message;
+           // textboxPanel.SetActive(true);
+        }
+        typingCoroutine = StartCoroutine(TypeText(message));
     }
 
     public void HideMessage()
     {
-        textboxPanel.SetActive(false);
+        TextBoxPanel.SetActive(false);
+    }
+
+    private IEnumerator TypeText(string message)
+    {
+        TextBoxPanel.SetActive(true);
+        messageText.text = "";
+
+        foreach (char letter in message.ToCharArray())
+        {
+            messageText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        yield return new WaitForSeconds(displayTime);
+
+        messageText.text = "";
+        TextBoxPanel.SetActive(false);
     }
 }
